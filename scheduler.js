1dddc3af9722ca5ef63aff125d32e25127a014e7
Anthony Lubin <anthonylubin@yahoo.co.uk>
2022-07-21 19:47:48 +0100

    pages, images and standard files

---
+++ scheduler.js
@@ -0,0 +1,10 @@
+const cron = require('node-cron');
+
+// Order: min, hour, day, month, day of week
+const task = cron.schedule(' 0 09-23/3 * * *', () => {
+    console.log("I'm running every 3 hours from 9am - 11pm")
+}, {timezone: "Europe/London"});
+
+task.start();
+
+module.exports = task;
