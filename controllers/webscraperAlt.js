3223ba93ad10a36d33ce19b511c7bc076f31b662
Anthony Lubin <anthonylubin@yahoo.co.uk>
2023-11-29 15:11:47 +0000

    changed for sainsburys & redsign extra

---
+++ controllers/webscraperAlt.js
@@ -0,0 +1,88 @@
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+const { ShoppingItem } = require('../models/product');
+
+puppeteer.use(StealthPlugin());
+
+const productSearched = async (searchItem) => {
+
+  const url = "https://www.sainsburys.co.uk/shop/gb/groceries";
+
+   // Launch the browser and open a new blank page
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  
+  page.setDefaultTimeout(50000);
+  page.setDefaultNavigationTimeout(50000);
+  await page.goto(url);
+  
+  // handle cookies modal
+  const cookiesBtn = await page.waitForSelector(
+    "button::-p-text(Accept All Cookies)"
+  );
+  await cookiesBtn.click();
+
+  // Enter product into search bar
+  const searchBar = await page.waitForSelector("#search");
+  await searchBar.type(searchItem);
+
+  await Promise.all([
+    page.waitForNavigation({ waitUntil: "load" }),
+    page.keyboard.press("Enter"),
+  ]);
+
+  // Click to product page
+  const productLink = await page.waitForSelector(".pt__link");
+  await Promise.all([
+    page.waitForNavigation({ waitUntil: "load" }),
+    productLink.click(),
+  ]);
+
+  // wait for image uploaded on page
+  await page.waitForSelector(".pd__image");
+
+  // Getting Info from searched product
+  // Using try/catch if dom element not available - handles thrown error 
+  const getProductInfo = async (selector, domProperty) => {
+    let productInfo;
+    try {
+      productInfo = await page.$eval(
+        selector,
+        (el, domAttribute) => {
+          return el[domAttribute];
+        },
+        domProperty // argument to params of eval function
+      );
+    } catch (error) {
+      console.log(error);
+      productInfo = "";
+    }
+    return productInfo;
+  };
+
+  // selectors for dom elements
+  const domRefs = {
+    title: "[role='main'] .pd__header",
+    offerPrice: "[role='main'] .pd__cost--price",
+    retailPrice: "[role='main'] .pd__cost__retail-price",
+    imageSrc: "[role='main'] .pd__image",
+    promotionMsg: "[role='main'] .promotion-message",
+    nectarOffer: "[role='main'] .promotional-tag--nectar",
+    offer: "[role='main'] .promotional-tag"
+  }
+
+  let Product = new ShoppingItem({
+    title: await getProductInfo(domRefs.title, "innerText"),
+    retailPrice: await getProductInfo(domRefs.retailPrice, "innerText"),
+    offerPrice: await getProductInfo(domRefs.offerPrice, "innerText"),
+    imageUrl: await getProductInfo(domRefs.imageSrc, "src"),
+    promotionMsg: await getProductInfo(domRefs.promotionMsg, "innerText"),
+    nectarOffer: await getProductInfo(domRefs.nectarOffer, "innerText"),
+    offer: await getProductInfo(domRefs.offer, "innerText")
+  });
+
+  browser.close();
+  return { Product };
+};
+
+module.exports = productSearched;
---
+++ routes/priceAlerts.js
@@ -0,0 +1,108 @@
+const express = require("express");
+const productSearched = require("../controllers/webscraperAlt");
+const sendEmail = require("../controllers/mailer");
+const { ShoppingItem } = require("../models/product");
+
+const router = express.Router();
+
+// HOME PAGE
+router.get("/", (_req, res) => {
+  res.render("pages/home.ejs");
+});
+
+//  UNSUBSCRIBE PAGE
+router.get("/unsubscribe", (_req, res) => {
+  res.render("pages/unsubscribe.ejs");
+});
+
+//  PRICE ALERT REMINDER PAGE
+router.get("/price-alerts-reminder", (_req, res) => {
+  res.render("pages/price-alerts-reminder.ejs");
+});
+
+//  HOW TO USE -DEMO PAGE
+router.get("/how-to-use", (_req, res) => {
+  res.render("pages/how-to-use.ejs");
+});
+
+//  RESULTS PAGE (SUBMIT REQUEST)
+router.post("/results-page-alt", async (req, res, next) => {
+  try {
+    // form details
+    let productName = req.body.search.toLowerCase();
+    let userEmail = req.body.emailAddress;
+
+    let { Product } = await productSearched(productName);
+    // Update Product object/schema & save to db
+    Product.email = userEmail;
+    await Product.save();
+    
+    console.log(
+      "\x1b[32m%s\x1b[0m",
+      "Well done! A price alert has been created."
+      );
+      
+    await sendEmail("pages/email-results.ejs", { Product, userEmail })
+      .then(() => {
+        res.status(200).render("pages/results-page-alt.ejs", { Product });
+      })
+
+  } catch (error) {
+    return next(error);
+  }
+});
+
+//  EMAIL PRICE ALERTS RETRIEVAL
+router.post("/price-alerts-reminder", async (req, res, next) => {
+  // email address
+  let userEmail = req.body.price_alerts;
+ 
+  // find documents(price alerts) associated with email address
+  try {
+    const userPriceAlerts = await ShoppingItem.find({ email: userEmail });
+    if (!userPriceAlerts.length) {
+      // pass user email to local response object to use in error handling
+      res.locals.userEmail = userEmail;
+
+      // set custom error status (code & message)
+      const error = new Error(
+        `No price alerts found associated with the email: ${userEmail}`
+      );
+      error.status = 404;
+      next(error);
+    } else {
+        await sendEmail("pages/email-price-alerts-reminders.ejs", { Product : userPriceAlerts, userEmail })
+        //  set time delay for response so that extra CSS can apply on client browser page
+        .then(() => setTimeout(() => res.status(200).redirect("/"), 3000));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+//  DELETE PRICE ALERT - unsubscribe from
+router.post("/unsubscribe", async (req, res, next) => {
+  // document Id for price alert
+  let priceAlertId = req.body.documentId;
+
+  try {
+    const deletedPriceAlert = await ShoppingItem.findByIdAndDelete(priceAlertId);
+    if (!deletedPriceAlert) {
+      // pass price alert id to response object for error handling
+      res.locals.priceAlertId = priceAlertId;
+
+      // set custom error status (code & message)
+      const error = new Error(
+        `No price alerts found associated with the ID: ${priceAlertId}`
+      );
+      error.status = 404;
+      next(error);
+    } else {
+      res.status(200).render("pages/success.ejs", { deletedPriceAlert });
+    }
+  } catch (error) {
+    next(error)
+  }
+});
+
+module.exports = router;
